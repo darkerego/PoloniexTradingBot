a48901981521961c76a8fe44c8b4c92332ea9558
tpc2102 <kriss13devil@gmail.com>
2017-07-12 21:38:59 +0700

    .

--- TradingBot.py
+++ TradingBot.py
@@ -2,6 +2,7 @@ import datetime
 import time
 import poloniex
 from poloniex import poloniex
+from test import test
 
 def createTimeStamp(datestr, format="%Y-%m-%d %H:%M:%S"):
     return time.mktime(time.strptime(datestr, format))
@@ -13,13 +14,17 @@ def sum(array):
 	return ret
 
 def main():
+	sellTarget = 0.0
 	interval = 30
 	pair = 'BTC_ETH'
 	data = poloniex('', '')
+	demo = test()
 	while True:
 		chartClose = []
 		timeNow = int(createTimeStamp('{:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now())))
-		lastPrice = data.returnTicker()[pair]['last']
+		if timeNow % 900 != 0:
+			timeNow = timeNow / 900 * 900
+		lastPrice = float(data.returnTicker()[pair]['last'])
 		chartData = data.returnChartData(pair, timeNow - 28800, timeNow, 900)
 		for candle in chartData:
 			chartClose.append(candle['close'])
@@ -29,12 +34,30 @@ def main():
 			ema1 += (chartClose[i] - ema1) * 2 / 17
 		for i in range(24,32):
 			ema2 += (chartClose[i] - ema2) * 2 / 9
-
+		buyTarget = (min(ema1, ema2) * 0.999)
 		#ema2 = data.returnChartData(pair, timeNow - 7200, timeNow, 900)[-1]['weightedAverage']
 		print '{:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now()) + ' %s :' % (pair)
 		print 'Last price : %s' %(lastPrice)
 		print 'EMA 1 : %s' %ema1
 		print 'EMA 2 : %s' %ema2
+		print 'Value : %s' %demo.value((lastPrice))
+		print 'Buy target : %s' %buyTarget
+		print 'Sell target : %s' %sellTarget
+		if demo.btc > 0:
+			if lastPrice < buyTarget:
+				demo.buy((lastPrice))
+				sellTarget = lastPrice * 1.0125
+				print 'Buy price : %s' %lastPrice
+				print 'Sell target : %s' %sellTarget
+		else:
+			if lastPrice > sellTarget:
+				demo.sell(lastPrice)
+				print 'Sell price : %s' %lastPrice
+				sellTarget = 0.0
+		print '***************************************************'
+
+
+
 
 		time.sleep(int(interval))
 	
--- test.py
+++ test.py
@@ -1,1 +1,16 @@
+class test():
+	btc = 1.0
+	alt = 0.0
 
+	def buy(self, price):
+		self.alt = self.btc / price
+		self.btc = 0
+		return 0
+
+	def sell(self, price):
+		self.btc = self.alt * price
+		self.alt = 0
+		return 0
+
+	def value(self, price):
+		return self.btc + self.alt * price
