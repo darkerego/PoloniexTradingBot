724cbf4ced8a94d6839bd218f66fbb28425cd5b6
tpc2102 <kriss13devil@gmail.com>
2017-07-12 13:34:47 +0700

    TradingBot.py

--- TradingBot.py
+++ TradingBot.py
@@ -1,10 +1,18 @@
+import datetime
+import time
 from poloniex import poloniex
 
 
+
 def main():
+	interval = 30
+	pair = 'BTC_ETH'
 	data = poloniex('', '')
+	
 
-	ret = data.returnTicker()
-
+	lastPrice = data.returnTicker()[pair]['last']
+	while True:
+		print '{:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now()) + ' %s Last price : %s' % (pair, lastPrice)
+		time.sleep(int(interval))
 	
 main()
